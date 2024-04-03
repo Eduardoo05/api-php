cbf567cb695d57553b26e067b650d762dcce2e66
Elvin Eduardo <franco193@hotmail.com>
2024-04-02 23:48:06 -0400

    se agregó archivo html al index

--- index.php
+++ index.php
@@ -3,3 +3,15 @@
 echo "hola";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php api</title>
+</head>
+<body>
+    <h1>hola mundo</h1>
+</body>
+</html>
